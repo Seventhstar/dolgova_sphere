3ef5a4580474059144a45bbb15597bb4c380e537
seventhstar <seventhstar@mail.ru>
2021-05-02 16:02:38 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'static/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'static/index', to: 'static#index'
   root to: "static#index" 
 end

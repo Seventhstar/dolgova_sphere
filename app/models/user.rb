815968de8dd11959050df9b3ab5a26062424ec67
seventhstar <seventhstar@mail.ru>
2021-05-07 11:42:54 +0300

    user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :add_info
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+#  resources :users, only: [:show]
   devise_for :users, ActiveAdmin::Devise.config
+#   devise_for :users, controllers: { registrations: 'users/registrations' }
   ActiveAdmin.routes(self)
   get 'team/index'
   get 'static/index', to: 'static#index'
+  get 'users/:id', to: 'user#show'
   root to: "static#index" 
 end
